611fc2ab5487ecb1b0f3f2a5fc2b065415185426
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-11-12 22:35:55 +0900

    stateの設計とクラスコンポーネントの作成

--- front/reactapp/src/App.js
+++ front/reactapp/src/App.js
@@ -1,7 +1,23 @@
-import './App.css';
+import React from 'react';
+import defaultDataset from './dataset';
+import './assets/styles/style.css';
 
-function App() {
-  return <p>a</p>;
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answers: [],
+      chats: [],
+      currentId: 'init',
+      dataset: defaultDataset,
+      open: false,
+    };
+  }
+  render() {
+    return (
+      <section className='c-section'>
+        <div className='c-box'>{this.state.currentId}</div>
+      </section>
+    );
+  }
 }
-
-export default App;

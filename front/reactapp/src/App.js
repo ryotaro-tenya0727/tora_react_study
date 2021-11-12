84f24eae61b0f158e445772ca102cc22ecc27b54
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2021-11-12 09:35:09 +0900

    react入門実践編の準備

--- front/reactapp/src/App.js
+++ front/reactapp/src/App.js
@@ -1,21 +1,7 @@
-import { useEffect } from 'react';
-// import { fetch } from './components/Todo';
 import './App.css';
-import Article from './components/Todo';
 
 function App() {
-  // useEffect(() => {
-  //   (async () => {
-  //     let a = await fetch();
-  //     console.log(a);
-  //   })();
-  // }, []);
-
-  return (
-    <>
-      <Article title={'みこたん'} content={'ウルム'} />
-    </>
-  );
+  return <p>a</p>;
 }
 
 export default App;
--- front/reactapp/src/components/PublishButton.jsx
+++
@@ -1,9 +0,0 @@
-const PublishButton = (props) => {
-  return (
-    <button onClick={() => props.onClick()}>
-      公開状態: {props.isPublished.toString()}
-    </button>
-  );
-};
-
-export default PublishButton;
